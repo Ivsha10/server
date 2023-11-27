e63f7bcbbf036eb96a04624d4fff2033c35e3645
Ivsha10 <duricicsolutions@gmail.com>
2023-11-27 16:23:15 -0500

    dbConn commit

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = process.env.URI;
+const URI = process.env.DATABASE_URI;
 const connectDB = async () => {
     try {
         await mongoose.connect(URI, {
